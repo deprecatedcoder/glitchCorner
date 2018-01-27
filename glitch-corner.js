ad0cd09c1b882677f0215e502203889a7ad2fdcb
Ryan Sullivan <RyanSullivan@gmail.com>
2018-01-27 15:30:33 -0500

    Change to log only final subProject value

--- glitch-corner.js
+++ glitch-corner.js
@@ -2,13 +2,12 @@ var project = window.location.hostname.replace('.glitch.me', '');
 
 var subProject = window.location.pathname.slice('1');
 
-console.log( "subProject: " + subProject);
-
 if (!subProject.includes(".htm"))
 {
   subProject = subProject + "/index.html";
-  console.log( "subProject: " + subProject);
 }
+
+console.log( "subProject: " + subProject);
 
 document.write( '<a href="https://glitch.com/edit/#!/' + project + '?path=' + subProject + '">' +
                 '<img width="40" height="26" src="https://glitch.com/edit/images/logos/glitch/logo-day.svg" style="position: absolute; top: 10; border: 0; right: 0; transform: rotate(45deg);">' +
